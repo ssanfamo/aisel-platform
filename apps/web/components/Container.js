1020669c406b97899121bf61c4acd70fcf2ef8fd
Sesan <sesanfamo@gmail.com>
2026-05-11 11:10:43 +0100

    After Images changes

--- apps/web/components/Container.js
+++ apps/web/components/Container.js
@@ -1,10 +1,12 @@
-export default function Container({ children, className = "" }) {
+export default function Container({
+  children,
+  className = "",
+}) {
   return (
     <div
-      className={`mx-auto w-full max-w-7xl px-6 lg:px-8 ${className}`}
+      className={`mx-auto w-full max-w-[1400px] px-6 lg:px-10 ${className}`}
     >
       {children}
     </div>
   );
 }
-
