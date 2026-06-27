623fb7954826f8f756def82d516a5ebbfef3d4a9
Sesan <sesanfamo@gmail.com>
2026-06-27 06:09:06 +0100

    Add Google Analytics GA4 tracking

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "../components/Navbar";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,6 +21,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} font-sans bg-white text-gray-900 antialiased`}>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PYMEKHB6XF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PYMEKHB6XF');
+          `}
+        </Script>
 
         <Navbar />
 
